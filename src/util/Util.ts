0fe99d9264bdf2ed480b29ea9d7663c1ec367d8e
dimwight <dimwight@ntlworld.com>
2017-08-11 19:48:57 +0100

    Util

---
+++ src/util/Util.ts
@@ -0,0 +1,23 @@
+import fn from 'date-fns/format';
+function format(time:Date,style:string){
+  return fn(time,style);
+  return 'late afternoon';
+}
+export function tellTheTime(){
+  const time=new Date();
+  const text=true?'Time is exactly '+format(time,'h:mm:ssa')
+    :'Time is about '+format(time,'h:mm');
+  if(typeof document==='undefined') console.log(text);
+  else{
+    document.getElementById('pageTitle').innerText=document.title;
+    document.querySelector('#time-now').textContent=text;
+    setTimeout(time,1000);
+  }
+}
+export function errorTest (msg?){
+  let err=new Error('Hell!');
+  console.log(`Created ${err} for ${msg}...`);
+  if(false) throw err;
+  else console.log(`..but didn't throw it.`);
+}
+
