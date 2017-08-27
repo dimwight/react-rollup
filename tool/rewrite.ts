5a5b80a5550a0b157ccdbde70462bceb1e172030
dimwight <dimwight@ntlworld.com>
2017-08-27 18:40:43 +0100

    rewrite in build

--- tool/rewrite.ts
+++ tool/rewrite.ts
@@ -5,7 +5,7 @@ console.log('Rewriting path='+path);
 const text=fs.readFileSync(path,).toString();
 const replace=text.replace(
   'var __extends = (this && this.__extends) || (function () {',
-  '(function () {'
+  'var __extends = (function () {'
 );
 if(false)console.log(replace);
 fs.writeFileSync(path,replace);
