564994d6f789223a3a851fe31fbf923fda727599
dimwight <dimwight@ntlworld.com>
2017-08-27 19:01:59 +0100

    esNext so no need for rewrite?

--- public/index.js
+++ public/index.js
@@ -4,23 +4,9 @@
 React = React && 'default' in React ? React['default'] : React;
 Dom = Dom && 'default' in Dom ? Dom['default'] : Dom;
 
-var __extends = (function () {
-    var extendStatics = Object.setPrototypeOf ||
-        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var Welcome = (function (_super) {
-    __extends(Welcome, _super);
-    function Welcome() {
-        return _super !== null && _super.apply(this, arguments) || this;
-    }
-    Welcome.prototype.render = function () {
-        var top = document.title + ' : : ';
+class Welcome extends React.Component {
+    render() {
+        const top = document.title + ' : : ';
         return (React.createElement("div", { className: "app" },
             React.createElement("div", { className: "app-header" },
                 React.createElement("h2", null, "Welcome to react-rollup")),
@@ -28,9 +14,8 @@ var Welcome = (function (_super) {
                 "Edited ",
                 React.createElement("code", null, (document.title = top + this.constructor.name).replace(top, '')),
                 ", perhaps for the last time?")));
-    };
-    return Welcome;
-}(React.Component));
+    }
+}
 
 // import {errorTest} from './util/Bits';
 // import {tellTheTime} from './util/Time';
