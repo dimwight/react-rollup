d49aac958f00a5df51ba6effb4fa4ed39677c877
dimwight <dimwight@ntlworld.com>
2017-09-01 19:16:00 +0100

    npm prune + update

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Dom from 'react-dom';
+import ReactDOM from 'react-dom';
 import {Welcome} from './welcome/Welcome';
 // import {errorTest} from './util/Bits';
 // import {tellTheTime} from './util/Time';
@@ -7,12 +7,10 @@ import {Welcome} from './welcome/Welcome';
 import './main.css';
 
 // errorTest();
-
-
 // tellTheTime();
 const title=document.getElementById('pageTitle');
 if(title!=null)title.innerText=document.title;
-Dom.render(<Welcome />,
+ReactDOM.render(<Welcome />,
   document.getElementById('app') as HTMLElement
 );
 
