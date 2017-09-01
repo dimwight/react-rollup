4fe88c2d7b74ea1efa644737cda3d23222127be6
dimwight <dimwight@ntlworld.com>
2017-09-01 19:18:41 +0100

    main to welcome

---
+++ src/welcome/main.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import {Welcome} from './welcome/Welcome';
+import {Welcome} from './Welcome';
+// import {errorTest} from './util/Bits';
+// import {tellTheTime} from './util/Time';
+
+import './main.css';
+
+// errorTest();
+// tellTheTime();
+const title=document.getElementById('pageTitle');
+if(title!=null)title.innerText=document.title;
+ReactDOM.render(<Welcome />,
+  document.getElementById('app') as HTMLElement
+);
+
