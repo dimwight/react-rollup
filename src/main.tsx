358c0c1c53ba744e520bc89cc4b786038fa82ded
dimwight <dimwight@ntlworld.com>
2017-09-01 20:28:50 +0100

    Flintstones

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,33 @@ import ReactDOM from 'react-dom';
 
 import './main.css';
 
+interface Flintstone{
+  first:FredOrWilma,
+  last:'Flintstone',
+}
+
+type FredOrWilma='Fred'|'Wilma';
+
+const user:Flintstone={
+  first:'Wilma',
+  last:'Flintstone',
+};
+
+function formatName(user){
+  if(typeof user=='string')return user;
+  return user.firstName+' '+user.lastName;
+}
+const element=(
+  <p>
+    Hello, {formatName(user)}!
+  </p>
+);
+function getGreeting(user?:FredOrWilma|Flintstone){
+  return <h1>Hello {user?formatName(user):' sailor'}!</h1>
+}
+
 ReactDOM.render(
-  <h1>Hello there</h1>,
-  document.getElementById('root')
+  getGreeting(user.first as FredOrWilma),
+  document.getElementById('root'),
 );
 
