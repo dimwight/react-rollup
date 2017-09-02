081184987082218bab3d40deb57a3a633ebf6672
dimwight <dimwight@ntlworld.com>
2017-09-02 09:19:44 +0100

    hello _exports

--- src/hello/hello.tsx
+++ src/hello/hello.tsx
@@ -1,34 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-interface Flintstone{
-  first:FredOrWilma,
-  last:'Flintstone',
-}
-
-type FredOrWilma='Fred'|'Wilma';
-
-const user:Flintstone={
-  first:'Wilma',
-  last:'Flintstone',
-};
-
-function formatName(user){
-  if(typeof user=='string')return user;
-  return user.firstName+' '+user.lastName;
-}
-const name=(
-  <p>
-    Hello, {formatName(user)}!
-  </p>
-);
-function getGreeting(user?:FredOrWilma|Flintstone){
-  return <h1>Hello {user?formatName(user):' sailor'}!</h1>
-}
-
 export function hello(){
+  interface Flintstone{
+    first:FredOrWilma,
+    last:'Flintstone',
+  }
+
+  type FredOrWilma='Fred'|'Wilma';
+
+  const user:Flintstone={
+    first:'Wilma',
+    last:'Flintstone',
+  };
+
+  function formatName(user){
+    if(typeof user=='string')return user;
+    return user.firstName+' '+user.lastName;
+  }
+  const name=(
+    <p>
+      Hello, {formatName(user)}!
+    </p>
+  );
+  function getGreeting(user?:FredOrWilma|Flintstone){
+    return <h1>Hello {user?formatName(user):' sailor'}!</h1>
+  }
   ReactDOM.render(
-    false?getGreeting(user.first as FredOrWilma)
+    true?getGreeting(user.first as FredOrWilma)
       :<h1>Hello</h1>,
     document.getElementById('root'),
   );
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,12 @@ import * as hello from './hello/_exports';
 
 import './main.css';
 
-if(true)hello.props();
-else if(true)hello.clock();
-else hello.hello();
+if(true){
+  hello.props();
+}
+else if(true){
+  hello.clock();
+}
+else {
+  hello.hello();
+}
