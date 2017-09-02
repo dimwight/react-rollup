bb32ce0de3b1093007259f1e78aa3c6d51ee46c6
dimwight <dimwight@ntlworld.com>
2017-09-02 20:19:58 +0100

    Just the data!

--- src/hellos/extract.tsx
+++ src/hellos/extract.tsx
@@ -1,46 +1,68 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {props} from './props';
 
-function formatDate(date) {
+function formatDate(date){
   return date.toLocaleTimeString();
 }
-function Comment(props) {
+function Avatar(props){
+  trace('Avatar',props);
+  return (
+    <img className="Avatar"
+         src={props.user.avatarUrl}
+         alt={props.user.name}
+    />
+  );
+}
+function trace(top,props){
+  console.log(top+': props='+JSON.stringify(props,null,1))
+}
+function UserInfo(props){
+  trace('UserInfo',props);
+  return (
+    <div className="UserInfo">
+      <Avatar user={props.user}/>
+      <div className="UserInfo-name">
+        {props.user.name}
+      </div>
+    </div>
+  );
+}
+function Data(props){
+  trace('Data',props);
+  return (
+    <div>
+      <div className="Comment-text">
+        {props.text.replace('Event and data binding with no events and no binding - j','J')}
+      </div>
+      <div className="Comment-date">{formatDate(props.date)}</div>
+    </div>
+  )
+}
+function Comment(props){
+  trace('Comment',props);
   return (
     <div className="Comment">
-      <div className="UserInfo">
-        <img className="Avatar"
-             src={props.author.avatarUrl}
-             alt={props.author.name} />
-        <div className="UserInfo-name">
-          {props.author.name}
-        </div>
-      </div>
-      <div className="Comment-text">
-        {props.text}
-      </div>
-      <div className="Comment-date">
-        {formatDate(props.date)}
-      </div>
+      <UserInfo user={props.author}/>
+      <Data text={props.text} date={props.date}/>
     </div>
   );
 }
 
 export function extract(){
   const base={
-    date: new Date(),
-    text: 'Event and data binding with no events and no binding!',
-    author: {
-      name: 'Facets:',
-      avatarUrl: 'http://superficial.sourceforge.net/Facets.jpg'
-    }
-  },
-  src = Object.assign({},base,{
-    text:base.text.replace('!',' - just the data!'),
-  });
+      date:new Date(),
+      text:'Event and data binding with no events and no binding!',
+      author:{
+        name:'Facets:',
+        avatarUrl:'http://superficial.sourceforge.net/Facets.jpg',
+      },
+    },
+    src=Object.assign({},base,{
+      text:base.text.replace('!',' - just the data!'),
+    });
   ReactDOM.render(
     false?Comment(base)
-    :<Comment date={src.date} text={src.text} author={src.author}/>,
-    document.getElementById('root')
+      :<Comment date={src.date} text={src.text} author={src.author}/>,
+    document.getElementById('root'),
   );
 }
