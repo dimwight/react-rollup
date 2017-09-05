133a14b83a28b31c7ec20d642e2425a1e522afd6
dimwight <dimwight@ntlworld.com>
2017-09-05 15:16:24 +0100

    events

--- src/hellos/events.tsx
+++ src/hellos/events.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function ActionLink() {
-  function handleClick(e) {
+function ActionLink(){
+  function handleClick(e){
     e.preventDefault();
     window.alert('Hi')
   }
@@ -12,7 +12,31 @@ function ActionLink() {
     </a>
   );
 }
+interface Toggling{
+  isSet
+}
+class ToggleButton extends React.Component<Toggling,Toggling>{
+  constructor(props){
+    super(props);
+    this.state={isSet:props.isSet};
+    this.onClick=this.onClick.bind(this);
+  }
+  onClick(){
+    this.setState(then=>({
+      isSet:!then.isSet,
+    }));
+  }
+  render(){
+    return (
+      <button onClick={this.onClick}>
+        {this.state.isSet?'On':'Off'}
+      </button>
+    );
+  }
+}
 export function events(){
-  ReactDOM.render(<ActionLink />,
+  ReactDOM.render(
+    false?<ActionLink/>
+      :<ToggleButton isSet={false}/>,
     document.getElementById('root'));
 }
